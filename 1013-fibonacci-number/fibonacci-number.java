9665663a0075b9e4a5fc4629605f7c68d79945fd
Lakshita Singh <lakshitasingh1106@gmail.com>
2026-02-25 21:39:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (31.45%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int fib(int n) {
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        if(n == 0){
+            return 0;
+        }
+        else if(n ==1){
+            return 1;
+        }
+        for(int i = 0;i<n-1;i++){
+            c = a+b;
+            a = b;
+            b = c;  
+        }
+        return c;
+    }
+}
